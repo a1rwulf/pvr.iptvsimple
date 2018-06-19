2ffb07d11e34f6881bef5787a8a2e8d193f67558
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2018-06-19 14:56:59 +0200

    PVR API 5.10.1
    (cherry picked from commit d8d34b2d863d45cc85ee058ff8c93178b0ddd05b)

--- src/client.cpp
+++ src/client.cpp
@@ -295,14 +295,27 @@ PVR_ERROR GetChannelStreamProperties(const PVR_CHANNEL* channel, PVR_NAMED_VALUE
   if (!channel || !properties || !iPropertiesCount)
     return PVR_ERROR_SERVER_ERROR;
 
-  if (*iPropertiesCount < 1)
+  if (*iPropertiesCount < 2)
     return PVR_ERROR_INVALID_PARAMETERS;
 
   if (m_data && m_data->GetChannel(*channel, m_currentChannel))
   {
     strncpy(properties[0].strName, PVR_STREAM_PROPERTY_STREAMURL, sizeof(properties[0].strName) - 1);
     strncpy(properties[0].strValue, m_currentChannel.strStreamURL.c_str(), sizeof(properties[0].strValue) - 1);
-    *iPropertiesCount = 1;
+    strncpy(properties[1].strName, PVR_STREAM_PROPERTY_ISREALTIMESTREAM, sizeof(properties[1].strName) - 1);
+    strncpy(properties[1].strValue, "true", sizeof(properties[1].strValue) - 1);
+    *iPropertiesCount = 2;
+    if (!m_currentChannel.properties.empty())
+    {
+      for (auto& prop : m_currentChannel.properties)
+      {
+        strncpy(properties[*iPropertiesCount].strName, prop.first.c_str(),
+                sizeof(properties[*iPropertiesCount].strName) - 1);
+        strncpy(properties[*iPropertiesCount].strValue, prop.second.c_str(),
+                sizeof(properties[*iPropertiesCount].strName) - 1);
+        (*iPropertiesCount)++;
+      }
+    }
     return PVR_ERROR_NO_ERROR;
   }
 
